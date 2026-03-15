1ae090157a73ae2b5335254047ed424f0682db27
Luca <locricchioluca@itis-molinari.eu>
2026-03-15 20:16:36 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-//const API_BASE_URL = "https://tuo-backend.onrender.com";
-const API_BASE_URL = "http://localhost:3000"
+const API_BASE_URL = "https://web-app-compito.onrender.com"
+//const API_BASE_URL = "http://localhost:3000"
