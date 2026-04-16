9e7948fdb450ef9cb9f823650e3cff7ec3986548
Luca <locricchioluca@itis-molinari.eu>
2026-04-16 11:26:48 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -291,7 +291,7 @@ app.post("/api/order", async (req, res) => {
             return res.status(400).json({ error: `Stock insufficiente per ${product.name}` })
         }
 
-        total += Number(product.price) * Number(item.quantity)
+        total = ( 0.15 * Number(product.price)) * Number(item.quantity)
         
 
         purchasedItems.push({
